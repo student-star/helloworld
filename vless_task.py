cf04440e6a1a1015128db54ee101082de8ace294
student-star <77377886+student-star@users.noreply.github.com>
2024-08-06 17:22:33 +0800

    Update vless_task.py

--- vless_task.py
+++ vless_task.py
@@ -1,14 +1,10 @@
 import paramiko
-import schedule
-import time
-import random
 import os
-from datetime import datetime, timedelta
 
 # Function to execute the remote command
 def execute_remote_command():
     hostname = "s8.serv00.com"
-    port = 60710
+    port = 60710  # Updated SSH port
     username = os.getenv("VPS_USERNAME")
     password = os.getenv("VPS_PASSWORD")
 
@@ -24,24 +20,20 @@ def execute_remote_command():
         ssh.connect(hostname, port, username, password)
         command = f"cd ~/domains/{username}.serv00.net/vless && ./check_vless.sh"
         stdin, stdout, stderr = ssh.exec_command(command)
+        print("STDOUT:")
         print(stdout.read().decode())
+        print("STDERR:")
         print(stderr.read().decode())
+    except paramiko.AuthenticationException:
+        print("Authentication failed, please verify your credentials.")
+    except paramiko.SSHException as sshException:
+        print(f"Unable to establish SSH connection: {sshException}")
+    except paramiko.BadHostKeyException as badHostKeyException:
+        print(f"Unable to verify server's host key: {badHostKeyException}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Operation error: {e}")
     finally:
         ssh.close()
 
-# Function to schedule the task at random times between 5 and 7 hours
-def schedule_random_task():
-    delay = random.randint(5 * 60 * 60, 7 * 60 * 60)  # Random delay in seconds
-    execution_time = datetime.now() + timedelta(seconds=delay)
-    print(f"Task scheduled to run at: {execution_time}")
-    schedule.every(delay).seconds.do(execute_remote_command)
-
-# Schedule the task
-schedule_random_task()
-
-# Run the scheduled task
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Execute the command
+execute_remote_command()
